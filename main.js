c15d4f5eb3a3fa44a2e6be1ac1d415cb796beab8
Sequita Bridges <sequita.bridges.luol@statefarm.com>
2021-12-08 10:56:35 -0700

    GREEN: get the text file to pop-up to download, but does not open in browswer

--- main.js
+++ main.js
@@ -4,9 +4,26 @@ import { s3Client } from "./client.js";
 
 // Set the parameters
 const params = {
-  Bucket: "<BUCKET_NAME>", // The name of the bucket. For example, 'sample_bucket_101'.
-  Key: "<FILENAME>", // The name of the object. For example, 'sample_upload.txt'.
-  Body: "<BODY_CONTENT>", // The content of the object. For example, 'Hello world!".
+  ACL: 'public-read',
+  Bucket: "sequita-bucket-01", // The name of the bucket. For example, 'sample_bucket_101'.
+  Key: "hello_sequita.txt", // The name of the object. For example, 'sample_upload.txt'.
+  Body: "Hello Everyone! It is a beautiful day!", // The content of the object. For example, 'Hello world!".
+  // s3: "x-amz-grant-full-control"
+  // BucketPolicy: {
+  //   "Version": "2012-10-17",
+  //   "Statement": [
+  //       {
+  //           "Sid": "public-read",
+  //           "Effect": "Allow",
+  //           "Principal": "*",
+  //           "Action": [
+  //               "s3:GetObject",
+  //               "s3:GetObjectVersion"
+  //           ],
+  //           "Resource": "arn:aws:s3:::a-very-unique-bucket-name-01/*"
+  //       }]
+  // }      
+  
 };
 
 const run = () => {
